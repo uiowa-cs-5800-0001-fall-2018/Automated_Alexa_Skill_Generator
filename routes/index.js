56ba01f730b48c1a07693cd061325eed3e582ff1
cedmnds <collin-edmonds@uiowa.edu>
2018-10-23 17:54:40 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+var mongoose = require('mongoose');
+
+
+
+
+// the schema is useless so far
+// we need to create a model using it
+
+
+
+
+module.exports = router;
