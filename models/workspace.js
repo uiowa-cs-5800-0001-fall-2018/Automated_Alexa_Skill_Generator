3105f9cf2b8fd409dbb374650bb6e6d50d4e44c2
CJ <collin-edmonds@uiowa.edu>
2018-11-28 15:01:43 -0600

    start of saving workspace feature

---
+++ models/workspace.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var SALT_WORK_FACTOR = 10;
+
+var workspaceSchema = new mongoose.Schema({
+    name: String,
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    workspace: String,
+    created_at: {type: Date, default: Date.now()}
+  });
+
+
+
+  // we need to create a model using it
+  var Workspace = mongoose.model('Workspace', workspaceSchema);
+  
+  // make this available to our users in our Node applications
+  module.exports = Workspace;
