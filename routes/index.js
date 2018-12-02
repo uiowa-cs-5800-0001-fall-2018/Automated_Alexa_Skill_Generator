5f0a84143bcf74cc220a62488fd96fce7d63583e
CJ <collin-edmonds@uiowa.edu>
2018-12-02 15:17:50 -0600

    re-removed console.logs in saveworkspace route

--- routes/index.js
+++ routes/index.js
@@ -201,8 +201,6 @@ router.get('/users/cool/', function(req, res, next) {
 router.post('/saveworkspace', function (req, res, next){
   var text = req.body.xml;
   var workspaceName = req.body.workspaceName;
-  //console.log(text);
-  console.log(workspaceName);
       if (!req.session.signed_in) {
         var err = new Error('Please log in to save a workspace!');
         err.status = 401;
@@ -215,14 +213,11 @@ router.post('/saveworkspace', function (req, res, next){
           email: req.session.email,
           workspace: text
         }
-        console.log('Right Here');
         Workspace.create(workspaceData, function (error, savedWorkspace) {
           if (error) {
-            console.log('error');
             return next(error);
           } 
           else {
-            console.log("Successfully created workspace: "+workspaceName);
             res.redirect('back');
           }
         });
