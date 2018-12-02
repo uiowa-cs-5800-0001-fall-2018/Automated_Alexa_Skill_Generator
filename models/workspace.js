328d7e5da5e6def850603bc5b07908beec0e871f
CJ <collin-edmonds@uiowa.edu>
2018-12-02 15:12:15 -0600

    changed unique values for workspace model

--- models/workspace.js
+++ models/workspace.js
@@ -4,8 +4,8 @@ var SALT_WORK_FACTOR = 10;
 
 var workspaceSchema = new mongoose.Schema({
     name: String,
-    username: { type: String, required: true, unique: true, trim: true },
-    email: { type: String, required: true, unique: true, trim: true },
+    username: { type: String, required: true, unique: false, trim: true },
+    email: { type: String, required: true, unique: false, trim: true },
     workspace: String,
     created_at: {type: Date, default: Date.now()}
   });
--- routes/index.js
+++ routes/index.js
@@ -197,6 +197,8 @@ router.get('/users/cool/', function(req, res, next) {
 router.post('/saveworkspace', function (req, res, next){
   var text = req.body.xml;
   var workspaceName = req.body.workspaceName;
+  //console.log(text);
+  console.log(workspaceName);
       if (!req.session.signed_in) {
         var err = new Error('Please log in to save a workspace!');
         err.status = 401;
@@ -209,16 +211,18 @@ router.post('/saveworkspace', function (req, res, next){
           email: req.session.email,
           workspace: text
         }
-        
-        
+        console.log('Right Here');
         Workspace.create(workspaceData, function (error, savedWorkspace) {
           if (error) {
+            console.log('error');
             return next(error);
           } 
           else {
+            console.log("Successfully created workspace: "+workspaceName);
+            res.redirect('back');
           }
         });
       }
-  res.redirect('/design');
+  //res.redirect('/design');
 });
 module.exports = router;
